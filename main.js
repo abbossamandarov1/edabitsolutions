752dded7d16cfc116dba6ccefadb4612e728eb75
abbos <obboshon22@gmail.com>
2022-06-17 10:25:15 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+function addition(a, b) {
+    return a + b
+}
+
+function convert(minutes) {
+    return minutes * 60
+}
+
+function addition(num) {
+    return ++num
+}
+
+function triArea(base, height) {
+    return (base * height) / 2
+}
+
+function calcAge(age) {
+    return age * 365
+}
+
+
+function remainder(x, y) {
+    return x % y
+}
+
+function points(twoPointers, threePointers) {
+    return (twoPointers * 2) + (threePointers * 3)
+}
+
+function lessThan100(a, b) {
+    if (a + b < 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function animals(chickens, cows, pigs) {
+    return (chickens * 2) + (cows * 4) + (pigs * 4)
+}
